becf985c6ce74b9de2d07486c46cdd9e36befc87
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-03-25 14:15:36 +0530

    basic example of lifecycle aware components

--- app/src/main/java/com/example/learn_lifecycle_awarecomponents/common_approach/LocationCommonActivity.kt
+++ app/src/main/java/com/example/learn_lifecycle_awarecomponents/common_approach/LocationCommonActivity.kt
@@ -1,16 +1,18 @@
 package com.example.learn_lifecycle_awarecomponents.common_approach
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.learn_lifecycle_awarecomponents.R
+import com.example.learn_lifecycle_awarecomponents.basic.SecondActivity
+import kotlinx.android.synthetic.main.activity_location_common.*
 
 class LocationCommonActivity : AppCompatActivity() {
 
     private var locationViewModel: LocationViewModel? = null;
-    private var connectionStatus: Boolean = false
     private var locationCommonListener: LocationCommonListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,35 +22,23 @@ class LocationCommonActivity : AppCompatActivity() {
         setupLocationListener()
         setUpViewModel()
 
+        btnLaunch.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+
     }
 
     fun setUpViewModel() {
         locationViewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
         locationViewModel?.locationStatus()?.observe(this, Observer { locationValue ->
             Log.i("-----> ", "" + locationValue)
-            connectionStatus = locationValue
-            locationCommonListener?.start()
+            locationCommonListener?.enable()
         })
     }
 
     private fun setupLocationListener() {
-        locationCommonListener = LocationCommonListener { location ->
+        locationCommonListener = LocationCommonListener(lifecycle) { location ->
             Log.d("---->", "Location is $location")
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (connectionStatus) {
-            locationCommonListener?.start()
-        }
-
-    }
-
-    override fun onStop() {
-        if (connectionStatus) {
-            locationCommonListener?.stop()
-        }
-        super.onStop()
     }
 }
--- app/src/main/java/com/example/learn_lifecycle_awarecomponents/common_approach/LocationCommonListener.kt
+++ app/src/main/java/com/example/learn_lifecycle_awarecomponents/common_approach/LocationCommonListener.kt
@@ -1,20 +1,51 @@
 package com.example.learn_lifecycle_awarecomponents.common_approach
 
 import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
-class LocationCommonListener(private val callback: (String) -> Unit) {
+class LocationCommonListener(lifecycle: Lifecycle, private val callback: (String) -> Unit) :
+    LifecycleObserver {
 
-    private var status = ""
+    private var myLifecycle: Lifecycle? = null
 
-    fun start() {
-        status = CONNECTED
-        callback.invoke("Somewhere in Los Angeles")
-        Log.i("-----> ", "start -> " + status)
+    init {
+        myLifecycle = lifecycle
+        myLifecycle?.addObserver(this)
     }
 
+    private var status = ""
+    var enabled = false
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun start() {
+        Log.i("----> ", "start() -> before if ")
+        if (enabled) {
+            status = CONNECTED
+            callback.invoke("Somewhere in Los Angeles")
+            Log.i("-----> ", "start -> " + status)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stop() {
-        status = DISCONNECTED
-        Log.i("-----> ", "stop -> " + status)
+        if (enabled) {
+            status = DISCONNECTED
+            Log.i("-----> ", "stop -> " + status)
+        }
+    }
+
+    fun enable() {
+        enabled = true
+        if (myLifecycle?.currentState?.isAtLeast(Lifecycle.State.STARTED)!!) {
+            start()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun destroy() {
+        myLifecycle?.removeObserver(this)
     }
 
     companion object {
